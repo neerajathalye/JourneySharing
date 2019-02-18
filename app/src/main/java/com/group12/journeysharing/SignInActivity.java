6d65bbc835b91b65ad862a9accd3412ec3495e96
neera <neerajathalye.1996@gmail.com>
2019-02-18 18:52:42 +0000

    Added Sign In and Sign Up Activities

---
+++ app/src/main/java/com/group12/journeysharing/SignInActivity.java
@@ -0,0 +1,37 @@
+package com.group12.journeysharing;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignInActivity extends AppCompatActivity {
+
+    Button button, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
