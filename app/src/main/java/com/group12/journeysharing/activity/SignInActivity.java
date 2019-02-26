8b66ae54ead4a6f7f334001b899a5adb80534f07
abbas443 <nadakkaa@tcd.ie>
2019-02-26 18:01:38 +0000

    Added Reset password email

--- app/src/main/java/com/group12/journeysharing/activity/SignInActivity.java
+++ app/src/main/java/com/group12/journeysharing/activity/SignInActivity.java
@@ -1,13 +1,19 @@
 package com.group12.journeysharing.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,6 +30,7 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     private EditText editTextEmail;
     private EditText editTextPassword;
     private Button signInButton;
+    private TextView forgotPassword;
 
 
 
@@ -41,9 +48,12 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         editTextPassword = findViewById(R.id.passwordField);
         signInButton = findViewById(R.id.buttonSignin);
         signUpButton = findViewById(R.id.buttonRegister);
-
+        forgotPassword = findViewById(R.id.forgotPassword);
         signInButton.setOnClickListener(this);
         signUpButton.setOnClickListener(this);
+        forgotPassword.setOnClickListener(this);
+
+
     }
 
     private void userLogin(){
@@ -85,6 +95,57 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             startActivity(intent);
 //            finish();
         }
+        if(view == forgotPassword) {
+
+            final AlertDialog.Builder alert = new AlertDialog.Builder(SignInActivity.this);
+            final EditText editText = new EditText(SignInActivity.this);
+            editText.setSingleLine();
+            FrameLayout container = new FrameLayout(SignInActivity.this);
+            FrameLayout.LayoutParams params = new  FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.leftMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+            params.rightMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+            params.topMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+            params.bottomMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+            editText.setLayoutParams(params);
+            container.addView(editText);
+            alert.setTitle("Enter Email");
+            alert.setView(container);
+            alert.setPositiveButton(R.string.send_email, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String email = String.valueOf(editText.getText());
+                    if (TextUtils.isEmpty(email)) {
+                        Toast.makeText(SignInActivity.this, "Please enter valid email address", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()) {
+                                    Toast.makeText(SignInActivity.this, "Please check your email account...", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(SignInActivity.this, SignInActivity.class));
+
+                                } else {
+                                    String message = task.getException().getMessage();
+                                    Toast.makeText(SignInActivity.this, "Error Occured: " + message, Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+
+                    }
+
+                }
+            });
+            alert.setNegativeButton(getString(R.string.cancel), null);
+            alert.create();
+            alert.show();
+
+
+        }
+
+
     }
 
 
