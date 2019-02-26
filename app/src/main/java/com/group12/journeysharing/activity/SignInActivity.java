10e115d32a7214237cab0348c7a65e34724047ae
abbas443 <nadakkaa@tcd.ie>
2019-02-26 14:24:00 +0000

    Sign In with firebase

--- app/src/main/java/com/group12/journeysharing/activity/SignInActivity.java
+++ app/src/main/java/com/group12/journeysharing/activity/SignInActivity.java
@@ -1,41 +1,87 @@
 package com.group12.journeysharing.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.group12.journeysharing.R;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button button, button2;
+
+
+    private Button SignUpButton;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button SignInButton;
+
+
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        button = findViewById(R.id.signUpButton);
-        button2 = findViewById(R.id.signInButton);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        editTextEmail = (EditText) findViewById(R.id.emailField);
+        editTextPassword = (EditText) findViewById(R.id.passwordField);
+        SignInButton = (Button) findViewById(R.id.buttonSignin);
+        SignUpButton = (Button) findViewById(R.id.buttonRegister);
+
+        SignInButton.setOnClickListener(this);
+        SignUpButton.setOnClickListener(this);
     }
+
+    private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)) {
+            //email is empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)) {
+            //password is empty
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //start profile activity
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == SignInButton){
+            userLogin();
+        }
+    }
+
+
+
 }
